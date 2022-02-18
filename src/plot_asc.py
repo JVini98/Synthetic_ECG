69c594a72b19c0c01814256080071d181dcf779b
JVini98 <jimmyviniegra@gmail.com>
2022-02-18 21:52:33 +0100

    plot .asc files ready

--- src/plot_asc.py
+++ src/plot_asc.py
@@ -1,6 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
-
+import ecg_plot
 
 file_path = '/home/jvini/Escritorio/deepfake-ecg/0.asc'
-ecg = np.loadtxt(file_path)
-print(ecg)
+ecg = np.loadtxt(file_path, dtype=None, delimiter=' ')
+ecg = ecg.T
+
+fig, axs = plt.subplots(8)
+
+for i in range(8):
+    axs[i].plot(ecg[i])
+
+plt.show()
