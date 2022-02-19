10b027344c97fdb33fd03d2bf75c5cd77ed97844
JVini98 <jimmyviniegra@gmail.com>
2022-02-19 04:07:07 +0100

    normalized the data

--- src/plot_asc.py
+++ src/plot_asc.py
@@ -6,9 +6,13 @@ file_path = '/home/jvini/Escritorio/deepfake-ecg/0.asc'
 ecg = np.loadtxt(file_path, dtype=None, delimiter=' ')
 ecg = ecg.T
 
+norm = np.linalg.norm(ecg)
+ecg = ecg/norm
+
 fig, axs = plt.subplots(8)
 
 for i in range(8):
     axs[i].plot(ecg[i])
 
+plt.savefig('represented_ecg.png', dpi=300)
 plt.show()
