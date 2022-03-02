3e12394367a5d427211b21b745d35dcd8a5385e3
JVini98 <jimmyviniegra@gmail.com>
2022-03-02 10:03:40 +0100

    Modified the code to import only data marked as AF

--- src/import_mat.py
+++ src/import_mat.py
@@ -1,31 +1,36 @@
 from scipy.io import loadmat
+import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-out_dir = "/home/jvini/PycharmProjects/TFG_ECG/formated_data"
+out_dir = "/home/jvini/PycharmProjects/TFG_ECG/formated_data_AF"
 os.makedirs(out_dir, exist_ok=True)
 
-for i in range(1,8528):
+df = pd.read_csv(r'/home/jvini/PycharmProjects/TFG_ECG/training2017/REFERENCE-original.csv')
+categories = df.values
 
-    if i < 10:
-        var = f'A0000{i}'
-    elif 10 <= i < 100:
-        var = f'A000{i}'
-    elif 100 <= i < 1000:
-        var = f'A00{i}'
-    elif 1000 <= i:
-        var = f'A0{i}'
+for i in range(1, 8528):
 
-    ecg = loadmat(f'training2017/{var}.mat')
-    ecg_array = ecg['val'][0]
+    if categories.item((i-1, 1)) == 'A':
+        if i < 10:
+            var = f'A0000{i}'
+        elif 10 <= i < 100:
+            var = f'A000{i}'
+        elif 100 <= i < 1000:
+            var = f'A00{i}'
+        elif 1000 <= i:
+            var = f'A0{i}'
 
-    file = open(f'{out_dir}/{i}.asc',"a")
+        ecg = loadmat(f'training2017/{var}.mat')
+        ecg_array = ecg['val'][0]
 
-    for line in ecg_array:
-        file.write(str(line))
-        file.write("\n")
-        file.flush()
+        file = open(f'{out_dir}/{i}.asc', "a")
+
+        for line in ecg_array:
+            file.write(str(line))
+            file.write("\n")
+            file.flush()
 
 
 
